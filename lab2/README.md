9e013ebfe18d9ad9b8408aa4ddfa27654c793e27
unknown <daixuan1996@163.com>
2015-03-12 20:09:31 +0800

    Add README.md and some maybe standard comments...

--- lab2/README.md
+++ lab2/README.md
@@ -1,7 +1,9 @@
 ### Author: Xuan Dai
----
 
+---
+  
+  
+#### Fisrt Version 2015/3/12
 #### The sorter script can run at following urls:
 > - [1](http://www.w3school.com.cn/tags/tag_table.asp "HTML <table>标签")
   - [2](http://my.ss.sysu.edu.cn/wiki/pages/viewpage.action?pageId=395968523 "Lab 01.环形菜单")
-
--- lab2/sorter.js
+++ lab2/sorter.js
@@ -1,3 +1,4 @@
+// init
 window.onload = function() {
     var tables = getAllTables();
     makeAllTablesSortable(tables);
@@ -18,7 +19,9 @@ function sortSingleTable(table) {
     var th = table.getElementsByTagName('th');
 
     for (var i = 0; i < th.length; i++) {
+        // bind events to the head of table
         addEvent(th[i], 'click', function(j) { return function() { sortColumn(j, table, sortWay(th[j])); }; }(i));
+        // use closure to avoid the i will always be referred as th.length
     }
 }
 
@@ -26,24 +29,27 @@ function addEvent(element, event, handler) {
     if (element.addEventListener) {
         element.addEventListener(event, handler, false);
     } else {
+        // for IE 8-
         element.attachEvent('on' + event, handler);
     }
 }
 
+// judge the sortting way
 function sortWay(th) {
-/* th：需要排序的列的th
- */
     var ascend = true, descend = false;
-    var th_class = th.className;
+    var th_class = th.className; // for save
 
+    // init by removing special classname('ascend' and 'descend')
     var ths = document.getElementsByTagName('th');
     for (var i = 0; i < ths.length; ++i) {
         ths[i].className = ths[i].className.replace( /(?:^|\s)ascend(?!\S)/g, '' );
         ths[i].className = ths[i].className.replace( /(?:^|\s)descend(?!\S)/g, '' );
     }
 
+
     th.className = th_class;
 
+    // for different cases
     if (th.className.match(/(?:^|\s)ascend(?!\S)/)) {
         th.className = th.className.replace( /(?:^|\s)ascend(?!\S)/g, '' );
         th.className += ' descend';
@@ -57,21 +63,23 @@ function sortWay(th) {
         return ascend;
     }
 }
-
-
-function sortColumn(col, table, way) {
-/* col: 根据此列排序
- * table: 需要排列的表
- * way: 排序方式（true: 升序，false: 降序）
+/**
+ * Sort tables according chosen column
+ * @param col   {number}           the chosen column
+ *        table {HTMLTableElement} the chosen table
+ *        way   {boolean}          the sortting way
  */
+function sortColumn(col, table, way) {
     var rows = [].slice.call(table.getElementsByTagName('tbody')[0].getElementsByTagName('tr'));
-    
+    // extra the rows in a array
+
     var rows_th, rows_th_class;
     if (rows[0].getElementsByTagName('th').length != 0) {
         rows_th = rows[0];
         rows_th_class = rows[0].className;
         rows.splice(0, 1);
     }
+    // for those whose don't have 'thead' tags
 
     rows.sort(function(row_a, row_b) {
         var col_a = row_a.getElementsByTagName('td')[col].innerHTML;
@@ -92,6 +100,8 @@ function sortColumn(col, table, way) {
         rows_th.className = rows_th_class;
         tbody.appendChild(rows_th);
     };
+    // for those whose don't have 'thead' tags
+
     for (var i = 0; i < rows.length; ++i) {
         tbody.appendChild(rows[i]);
     }
